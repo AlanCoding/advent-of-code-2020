5675ab98ec0862b9c8abd803322bf22be99f18eb
Caleb Boylan <calebboylan@gmail.com>
2020-12-05 18:08:19 -0800

    Add day3 solution

---
+++ day3/src/main.rs
@@ -0,0 +1,74 @@
+use std::env;
+use std::fs;
+
+fn load_input(filename: &str) -> String {
+    fs::read_to_string(filename).expect("Reading file failed")
+}
+
+fn parse_input<'a>(data: &'a str) -> impl Iterator<Item = &'a str> + 'a + Clone {
+    data.lines()
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = if args.len() >= 2 {
+        args[1].clone()
+    } else {
+        "input".to_string()
+    };
+    let input = load_input(&filename);
+    let data = parse_input(&input);
+
+    let tree_count = data
+        .clone()
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(i * 3).unwrap();
+            c == '#'
+        })
+        .count();
+    println!("part1: {}", tree_count);
+
+    let tc1 = data
+        .clone()
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(*i).unwrap();
+            c == '#'
+        })
+        .count();
+    let tc2 = data
+        .clone()
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(i * 3).unwrap();
+            c == '#'
+        })
+        .count();
+    let tc3 = data
+        .clone()
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(i * 5).unwrap();
+            c == '#'
+        })
+        .count();
+    let tc4 = data
+        .clone()
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(i * 7).unwrap();
+            c == '#'
+        })
+        .count();
+    let tc5 = data
+        .clone()
+        .step_by(2)
+        .enumerate()
+        .filter(|(i, l)| {
+            let c = l.chars().cycle().nth(*i).unwrap();
+            c == '#'
+        })
+        .count();
+    println!("part1: {}", tc1 * tc2 * tc3 * tc4 * tc5);
+}
